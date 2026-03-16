57ece63e7ebe04cdbdbe2e487eabd4431e842b26
Moataz0000 <moatazfawzy150@gmail.com>
2026-03-16 02:59:32 +0200

    updates

--- Behavioral/observer.py
+++ Behavioral/observer.py
@@ -1,60 +1,39 @@
-from abc import ABC, abstractmethod
 from typing import List
+"""
+The Observer Design Pattern is a behavioral design pattern
+used when one object needs to notify many other objects automatically whenever its state changes.
 
+The Design Structure
+    - Subject (Publisher)
+        - Keeps a list of observers
+        - Notifies them when something changes
+    - Observer (Subscriber)
+        - Receives updates
+"""
 
-class Observer(ABC):
-    """Abstract Observer interface"""
-    @abstractmethod
-    def update(self, message: str) -> None:
+class Observer:
+    def update(self, subject: Subject) -> None:
         pass
 
 
-class EmailNotifier(Observer):
-    """Subject: Profile"""
-    def __init__(self, recipient_email: str):
-        self.recipient_email = recipient_email
 
-    def update(self, message: str) -> None:
-        print(f"Sending Email to {self.recipient_email}: {message}")
+class Subject:
+    _observers = List[Observer]
 
+    def __init__(self) -> None:
+        self._observers = []
 
-class Profile:
-    def __init__(self):
-        self._observers: List[Observer] = []
-        self._name: str = ""
     
     def attach(self, observer: Observer) -> None:
         if observer not in self._observers:
             self._observers.append(observer)
+    
+    def detach(self, observer: Observer):
+        try:
+            self._observers.remove(observer)
+        except ValueError:
+            pass
 
-    def detach(self, observer: Observer) -> None:
-        self._observers.remove(observer)
-
-    def notify(self, message: str) -> None:
+    def notify(self) -> None:
         for observer in self._observers:
-            observer.update(message)
-
-    def create_profile(self, name: str) -> None:
-        self._name = name
-        print(f"Profile created for {self._name}")
-        self.notify(f"New profile created for {self._name}")
-
-
-def main():
-    profile = Profile()
-
-    email_notification1 = EmailNotifier(recipient_email="mezo@gmail.com")
-    email_notification2 = EmailNotifier(recipient_email="moataz@gmail.com")
-
-    profile.attach(email_notification1)
-    profile.attach(email_notification2)
-
-    profile.create_profile("Mezo Da Ana")
-
-    profile.detach(email_notification2)
-    print("\nCreating another profile...")
-    profile.create_profile("Jane Smith")
-
-
-if __name__ == "__main__":
-    main()
+            observer.update(self)
